02dc06a340c63a4cf8083a04fac7999372d47b20
Bakare Aminat <aminatdamilola50@gmail.com>
2023-05-22 14:55:43 -0700

    Gather data

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,11 +1,6 @@
 #!/usr/bin/python3
-<<<<<<< HEAD
-"""python scripts that uses REST API for a given employee ID, returns information about
-their TODO list progress"""
-=======
-"""Python scripts that uses REST API for a given employee ID, returns
-information aboutPython scripts that uses REST API ftheir TODO list progress"""
->>>>>>> a3a8bfad842149c311517abfdfc6694f008b209b
+"""python scripts that uses REST API for a given employee ID, returns
+information about their TODO list progress"""
 
 import requests
 import sys
@@ -15,10 +10,7 @@ if __name__ == "__main__":
     userId = sys.argv[1]
     user = requests.get("https://jsonplaceholder.typicode.com/users/{}"
                         .format(userId))
-<<<<<<< HEAD
 
-=======
->>>>>>> a3a8bfad842149c311517abfdfc6694f008b209b
     name = user.json().get('name')
 
     todos = requests.get('https://jsonplaceholder.typicode.com/todos')
@@ -28,7 +20,7 @@ if __name__ == "__main__":
     for task in todos.json():
         if task.get('userId') == int(userId):
             totalTasks += 1
-<<<<<<< HEAD
+
             if task.get('completed'):
                 completed += 1
 
@@ -37,16 +29,3 @@ if __name__ == "__main__":
 
     print('\n'.join(["\t " + task.get('title') for task in todos.json()
           if task.get('userId') == int(userId) and task.get('completed')]))
-=======
-
-            if task.get('completed'):
-                completed += 1
-
-                print('Employee {} is done with tasks({}/{}):'
-                      .format(name, completed, totalTasks))
-
-                print('\n'.join(["\t " + task.get('title') for
-                      task in todos.json()
-                      if task.get('userId') == int(userId) and
-                      task.get('completed')]))
->>>>>>> a3a8bfad842149c311517abfdfc6694f008b209b
